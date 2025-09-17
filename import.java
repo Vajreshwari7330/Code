52268066302331e58b2e0ea3ca4f40498cad5ef2
Vajreshwari7330 <vajreshwarigoulikar053@gmail.com>
2025-09-17 19:21:03 +1300

    commit message

---
+++ import.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class DivisionCalculator
+extends jFrame{
+private jtextfield num1fiend,
+num2field,resultfiend;
+private jButton divideButton;
+public divisionCalculator(){
+//Frame title
+setTitle("division Calculator");
+setTsize(400,200);
+setDefaltcloseoperation(JFame.EXIT_ON_CbOSE);
+setLayout(new GridLayd
